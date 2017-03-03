f8ecc62211cc1a04499d446ab62a564b87c88369
Maciej Nowak <mcnowak@wp.pl>
2017-03-03 23:12:34 +0100

    Initial stage

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(function(){
+	var url = 'https://restcountries.eu/rest/v1/name/',
+		countriesList = $('#countries');
+		
+	function searchCountries() {
+		var countryName = $('#country-name').val();
+		if(!countryName.length) countryName = 'Poland';
+		$.ajax({
+			url: url + countryName,
+			method: 'GET',
+			success: showCountriesList
+		});
+	};
+
+	function showCountriesList(resp) {
+		countriesList.empty();
+		resp.forEach(function(item){
+			$('<li>').text(item.name + ' - ' + item.capital).appendTo(countriesList);
+		});
+	};
+
+	$('#search').click(searchCountries);
+});
